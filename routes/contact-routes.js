a410c5c0da2b1f2cc8743cc3b9165dabf5512ec6
Akim <202@mail.com>
2023-06-28 16:11:16 +0300

    project Node

---
+++ routes/contact-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { getContacts } = require('../controller/contact-controller');
+
+const router = express.Router();
+
+router.get('/contacts', getContacts);
+
+module.exports = router;
